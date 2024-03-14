832f14e32f53e78a97c503ff0079a96db25ab9a1
Do Alexis <alexis.do@epfl.ch>
2024-03-14 02:14:05 +0100

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let settingsMenu = document.querySelector('.settings-menu');
+let darkbtn = document.getElementById('dark-btn');
 
 function settingsMenuToggle(){
     settingsMenu.classList.toggle('settings-menu-height');
 }
 
+darkbtn.onclick = function () {
+     darkbtn.classList.toggle('dark-btn-activated') ;
+     document.body.classList.toggle('dark-theme');
+    
+     if(localStorage.getItem("theme") === "light"){
+        localStorage.setItem("theme","dark");
+     }else{
+        localStorage.setItem("theme","light");
+     }
+    
+    }
+
+if(localStorage.getItem("theme") === "light"){
+    darkbtn.classList.remove('dark-btn-activated');
+    document.body.classList.remove('dark-theme')
+}
+else if(localStorage.getItem("theme")=== "dark"){
+    darkbtn.classList.add('dark-btn-activated');
+    document.body.classList.add('dark-theme');
+    
+}else{
+    localStorage.setItem("theme","light")
+}
